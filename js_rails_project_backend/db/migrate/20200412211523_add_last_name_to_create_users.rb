13a384f594ed294f671db44f705670cf2fe791bf
Grace S <vocalise@gmail.com>
2020-04-12 14:26:51 -0700

    Added user migrations

---
+++ js_rails_project_backend/db/migrate/20200412211523_add_last_name_to_create_users.rb
@@ -0,0 +1,5 @@
+class AddLastNameToCreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :last_name, :string
+  end
+end
---
+++ js_rails_project_backend/db/migrate/20200412211659_change_column_name_to_users.rb
@@ -0,0 +1,5 @@
+class ChangeColumnNameToUsers < ActiveRecord::Migration[5.2]
+  def change
+    rename_column :users, :name, :first_name
+  end
+end
