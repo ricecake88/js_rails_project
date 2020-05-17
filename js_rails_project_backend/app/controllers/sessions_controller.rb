8f5400e99b5caf0722b9fda7dbeed99fd48f27d8
Grace S <vocalise@gmail.com>
2020-05-17 11:52:19 -0700

    Added def create to session controller to create session - not tested yet

--- js_rails_project_backend/app/controllers/sessions_controller.rb
+++ js_rails_project_backend/app/controllers/sessions_controller.rb
@@ -1,3 +1,16 @@
 class SessionsController < ApplicationController
+    def new
+        render json: {status: 'test'}
+    end
+
+    def create
+        user = User.find_by(:email => params[:email])
+        if user && user.authenticate(params[:password])
+          sessions[:user_id] = user.id
+          render json: user, include: [params[:first_name]];
+        else
+          render json: {status: false}
+        end
+    end
 
 end
