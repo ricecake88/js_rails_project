68b6765217469d807e57eef8b645db03d30e5382
Grace S <vocalise@gmail.com>
2020-05-17 17:48:03 -0700

    Added create action and post signup route to routes

--- js_rails_project_backend/app/controllers/users_controller.rb
+++ js_rails_project_backend/app/controllers/users_controller.rb
@@ -1,3 +1,21 @@
 class UsersController < ApplicationController
 
+    def create
+        user = User.find_by(:email => params[:email_address])
+        if !user.present?
+            user = User.new(user_params)
+            if user.save
+                session[:user_id] = user.id
+                render json: {status: true, first_name: user.first_name }
+            end
+        else
+            render json: {status: false, message: "User already exists at this e-mail address"};
+        end
+    end
+
+    private
+    def user_params
+        params.permit(:first_name, :last_name, :email_address, :password)
+    end    
+
 end
--- js_rails_project_backend/config/routes.rb
+++ js_rails_project_backend/config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/test', to: 'application#test'
   post '/login', to: 'sessions#create'
+  post '/signup', to: 'users#create'
 end
