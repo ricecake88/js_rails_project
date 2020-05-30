e4cdbee1ff533bb84bc5479a9710f06f1f427f07
Grace S <vocalise@gmail.com>
2020-05-30 16:36:33 -0700

    Updated application controller with cookies and forgery protection

--- js_rails_project_backend/app/controllers/application_controller.rb
+++ js_rails_project_backend/app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::API
+    include ActionController::Cookies
+    include ActionController::RequestForgeryProtection
+
+    #protect_from_forgery with: :exception
+    #protect_from_forgery with :null_session
+
     def test
         render json: { test: "success" }
     end
