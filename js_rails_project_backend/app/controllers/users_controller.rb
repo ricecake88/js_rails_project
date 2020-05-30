3b695dca5b5a9c8038a2189bf71a0df9bc91f011
Grace S <vocalise@gmail.com>
2020-05-30 16:09:33 -0700

    Updated render json for user create

--- js_rails_project_backend/app/controllers/users_controller.rb
+++ js_rails_project_backend/app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
             user = User.new(user_params)
             if user.save
                 session[:user_id] = user.id
-                render json: {status: true, first_name: user.first_name }
+                render json: {status:true, first_name: user.first_name, last_name: user.last_name, email:user.email }
             end
         else
             render json: {status: false, message: "User already exists at this e-mail address"};
