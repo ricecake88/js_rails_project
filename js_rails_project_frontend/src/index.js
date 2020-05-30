53d8506ed445466c502678c55010ff23e67f1a11
Grace S <vocalise@gmail.com>
2020-05-30 16:28:11 -0700

    Update index.js to check if user is logged in and beginning habit form

--- js_rails_project_frontend/src/index.js
+++ js_rails_project_frontend/src/index.js
@@ -4,8 +4,20 @@ fetch(`${BACKEND_URL}/test`)
   .then(json => console.log(json));
 
 
+function habit_interface() {
+  const bodyElement = document.querySelectorTag("body");
+
+  const habitForm = document.createElement("form")
+  bodyElement.appendChild(habitForm);
+
+}
+
 window.addEventListener('DOMContentLoaded', (event) => {
   console.log('DOM_fully loaded and parsed');
   User.handle_login();
   User.monitor_signup_link();
+  if (User.logged_in()) {
+    // show interface to add a habit
+    habit_interface();
+  }
 })
