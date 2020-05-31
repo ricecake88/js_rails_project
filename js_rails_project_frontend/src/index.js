f4bf7a642c0c50495d52658f3e513b76abd3bc5d
Grace S <vocalise@gmail.com>
2020-05-31 16:37:24 -0700

    Removed habit information in index.js

--- js_rails_project_frontend/src/index.js
+++ js_rails_project_frontend/src/index.js
@@ -4,20 +4,14 @@ fetch(`${BACKEND_URL}/test`)
   .then(json => console.log(json));
 
 
-function habit_interface() {
-  const bodyElement = document.querySelectorTag("body");
-
-  const habitForm = document.createElement("form")
-  bodyElement.appendChild(habitForm);
-
-}
-
 window.addEventListener('DOMContentLoaded', (event) => {
   console.log('DOM_fully loaded and parsed');
-  User.handle_login();
-  User.monitor_signup_link();
-  if (User.logged_in()) {
+  let new_user = new User();
+  new_user.handle_login();
+  new_user.monitor_signup_link();
+  if (new_user.login_state) {
     // show interface to add a habit
-    habit_interface();
+    console.log("yay logged in");
+    Habit.add_habit_form();
   }
 })
