1f502930e7a746eb96eff13b5f95c52f9e292d68
Grace S <vocalise@gmail.com>
2020-06-21 14:27:54 -0700

    Separated methods into sub functions in user.js - also split up fetch due to asynchronous events

--- js_rails_project_frontend/src/user.js
+++ js_rails_project_frontend/src/user.js
@@ -6,122 +6,116 @@ class User {
         this.login_state = login_state;
     }
 
-    function handle_login() {
-        const login = document.getElementById('login');
-        login.addEventListener('submit', function(e) {
-            e.preventDefault();
-            alert("Button pressed!");
+    /* getter for login_state */
+    get login_state() {
+        return this._login_state;
 
-            let email = document.querySelector("#login_submit_form input#email").value;
-            console.log(email);
-            let password = document.querySelector("#login_submit_form input#password").value
-            console.log(password);
-
-            let configObject = {
-                method: 'post',
-                headers: {
-                    "Content-Type": "application/json",
-                    "Accept": "application/json"
-                },
-                body: JSON.stringify({
-                    'email': email,
-                    'password': password
-                })
-            };
-            console.log(configObject);
-            fetch(`${BACKEND_URL}/login`, configObject)
-            //.then(response => response.json(localStorage.setItem("loggedIn", true))
-            .then(response => response.json())
-            .then(json => {
-                console.log(json);
-                const userElement = document.querySelector('#user');
-                const textElement = document.createElement('p');
-              if (!json['status']) {
-                textElement.textContent = "Login Failed";
-                userElement.appendChild(textElement);
-              } else { //login passed
-               textElement.textContent = "Welcome, " + json['first_name'];
-               userElement.appendChild(textElement);
-               login.setAttribute("class", "hidden");
-               this.login_state = true;
-              }
-            })
-            .catch(function(error) {
-                console.log(error);
-            });
-        })
     }
 
-    function logged_in() {
-        if (this.login_state == true) {
-            console.log("User is logged in");
-            return true;
+    /* setter for login_state */
+    set login_state(state) {
+        this._login_state = state;
+    }
+
+    fetch_json(action_path, configObject) {
+        return fetch(action_path, configObject)
+        .then(response => response.json())
+        .then(json => json)
+    }
+
+    /* creates config object from retrieved information
+    /*    from login form */
+    createLoginConfigObject() {
+        alert("Button pressed!");
+        let login_ok = false;
+
+        let email = document.querySelector("#login_submit_form input#email").value;
+        let password = document.querySelector("#login_submit_form input#password").value
+
+        let configObject = {
+            method: 'post',
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                'email': email,
+                'password': password
+            })
+        };
+        return configObject;
+    }
+
+    /* handles return json from login state */
+    handle_login_info(json) {
+
+        const userElement = document.querySelector('#user');
+        const textElement = document.createElement('p');
+
+        /* if login was successful, it welcomes the user
+         * and hides the login form,  otherwise
+         * it says that login is failed */
+        if (!json['status']) {
+            textElement.textContent = "Login Failed";
+            userElement.appendChild(textElement);
+        } else { //login passed
+            textElement.textContent = "Welcome, " + json['first_name'];
+            userElement.appendChild(textElement);
+            login.setAttribute("class", "hidden");
+        }
+
+    }
+
+//            .then(response => response.json(localStorage.setItem("loggedIn", true))
+
+    createSignupConfigObj() {
+        this.hide_login();
+
+        // get info from form
+        const email = document.querySelector("#signup_form input#email").value;
+        const first_name = document.querySelector("#signup_form input#first_name").value;
+        const last_name = document.querySelector("#signup_form input#last_name").value;
+        const password = document.querySelector("#signup_form input#password").value;
+        const password_confirmation = document.querySelector("#signup_form input#password").value;
+
+        if (password !== password_confirmation) {
+            console.log("Passwords do not match");
         } else {
-            console.log("User is not logged in");
-            return false;
+            console.log("They match!");
+        }
+
+        // create configObject from form input
+        let configObject = {
+            method: 'post',
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                'email': email,
+                'first_name': first_name,
+                'last_name': last_name,
+                'password': password
+            }),
+        };
+
+        console.log(configObject);
+        return configObject;
+    }
+
+    handleSignupInfo(json) {
+
+        if (json['status']) {
+            document.getElementById('user').innerHTML = "Welcome, " + json.first_name;
+            //this.first_name = json.first_name;
+            //this.last_name = json.last_name;
+            //this.email_address = json.email;
+        } else {
+            document.getElementById('user').innerHTML = json.message;
         }
     }
 
-    static monitor_signup_link(e) {
-        const signup_button = document.querySelector("button#signup_link");
-        signup_button.addEventListener("click", function(e) {
-            e.preventDefault();
-            document.getElementById('user').innerHTML = document.getElementById('signup').innerHTML;
-            User.hide_login();
-            const signup_form = document.getElementById('signup_form');
-            signup_form.addEventListener("submit", function(e) {
-                e.preventDefault();
-                // get info from form
-                const email = document.querySelector("#signup_form input#email").value;
-                console.log(email);
-                const first_name = document.querySelector("#signup_form input#first_name").value;
-                console.log(first_name);
-                const last_name = document.querySelector("#signup_form input#last_name").value;
-                console.log(last_name)
-                const password = document.querySelector("#signup_form input#password").value;
-                console.log(password)
-                const password_confirmation = document.querySelector("#signup_form input#password").value;
-                if (password !== password_confirmation) {
-                    console.log("Passwords do not match");
-                } else {
-                    console.log("They match!");
-                }
-                // add function to handle errors
-
-                let configObject = {
-                    method: 'post',
-                    headers: {
-                        "Content-Type": "application/json",
-                        "Accept": "application/json"
-                    },
-                    body: JSON.stringify({
-                        'email': email,
-                        'first_name': first_name,
-                        'last_name': last_name,
-                        'password': password
-                    }),
-                };
-                fetch(`${BACKEND_URL}/signup`, configObject)
-                .then(response => response.json())
-                .then(json => {
-                    if (json.status) {
-                        document.getElementById('user').innerHTML = "Welcome, " + json.first_name;
-                        this.first_name = json.first_name;
-                        this.last_name = json.last_name;
-                        this.email_address = json.email;
-                    } else {
-                        document.getElementById('user').innerHTML = json.message;
-                    }
-                })
-                .catch(function(error) {
-                    alert("Bad!");
-                    console.log(error.message);
-                })
-            })
-        })
-    }
-
-    function hide_login() {
+    hide_login() {
         const login_form = document.getElementById('login');
         login_form.setAttribute("class", "hidden");
     }
