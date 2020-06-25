c6478acc0bec52bc10b8420e231c2b2dc5500c22
Grace S <vocalise@gmail.com>
2020-06-24 22:52:39 -0700

    Updated add_habit_form for habit.js

--- js_rails_project_frontend/src/habit.js
+++ js_rails_project_frontend/src/habit.js
@@ -8,6 +8,7 @@ class Habit {
     }
 
     static add_habit_form() {
+        console.log("In add_habit_form");
         const bodyElement = document.querySelector("body");
 
         const habitForm = document.createElement("form")
@@ -16,17 +17,41 @@ class Habit {
         labelForHabitNameElement.setAttribute("for", "habitName");
         labelForHabitNameElement.textContent = "Habit Name: ";
 
-        const inputHabitNameElement = document.createElement("input")
+        const inputHabitNameElement = document.createElement("input");
         inputHabitNameElement.setAttribute("id", "habitName");
         inputHabitNameElement.setAttribute("name", "habitName");
+        inputHabitNameElement.setAttribute("type", "text");
+
+        const labelForScheduleElement = document.createElement("label");
+        labelForScheduleElement.setAttribute("for", "frequency");
+        labelForScheduleElement.textContent = "Frequency";
+
+        const scheduleElement = document.createElement("select");
+        scheduleElement.setAttribute("name", "frequency");
+        scheduleElement.setAttribute("id", "frequency");
+        const optionOne = document.createElement("option");
+        optionOne.setAttribute("value", "1");
+        optionOne.textContent = "Everyday";
+        const optionTwo = document.createElement("option");
+        optionTwo.setAttribute("value", "2");
+        optionTwo.textContent = "Every Other Day";
+        scheduleElement.appendChild(optionOne);
+        scheduleElement.appendChild(optionTwo);
+
 
         const submitElement = document.createElement("submit");
+        submitElement.setAttribute("name", "submitHabit");
+        submitElement.setAttribute("id", "submitHabit");
 
 
         habitForm.appendChild(labelForHabitNameElement);
         habitForm.appendChild(inputHabitNameElement);
+        habitForm.appendChild(labelForScheduleElement);
+        habitForm.appendChild(scheduleElement);
+        habitForm.appendChild(submitElement);
       
         bodyElement.appendChild(habitForm);
+        console.log(habitForm);
     }
 
 }
