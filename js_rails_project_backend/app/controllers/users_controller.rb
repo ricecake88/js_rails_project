eacc739f4b75100ec0c5fa016caa2e6724374bee
Grace S <vocalise@gmail.com>
2020-09-22 22:14:19 -0700

    Changed user_params and error json message for user controller

--- js_rails_project_backend/app/controllers/users_controller.rb
+++ js_rails_project_backend/app/controllers/users_controller.rb
@@ -9,13 +9,13 @@ class UsersController < ApplicationController
                 render json: {status:true, first_name: user.first_name, last_name: user.last_name, email:user.email }
             end
         else
-            render json: {status: false, message: "User already exists at this e-mail address"};
+            render json: {status: false, message: "User already exists at this e-mail address"}
         end
     end
 
     private
     def user_params
-        params.permit(:first_name, :last_name, :email, :password)
+        params.permit(:firstName, :lastName, :email, :password)
     end    
 
 end
