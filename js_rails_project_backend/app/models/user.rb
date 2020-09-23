0e0dd3d902b7433b0dc4714f8b07194441ab5cb2
Grace S <vocalise@gmail.com>
2020-09-22 22:27:10 -0700

    Added validation checks for User Model

--- js_rails_project_backend/app/models/user.rb
+++ js_rails_project_backend/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
+    validates :first_name, :last_name, presence: true
+    validates :email, presence: true, uniqueness: true, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
     has_many :habits
 end
