fd18cc1e4a290cdb17109c194edf1e6cd51b96d0
Grace S <vocalise@gmail.com>
2020-10-18 14:30:18 -0700

    Added application controller actions

--- js_rails_project_backend/app/controllers/application_controller.rb
+++ js_rails_project_backend/app/controllers/application_controller.rb
@@ -1,11 +1,35 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
     include ActionController::RequestForgeryProtection
+    before_action :current_user
 
     #protect_from_forgery with: :exception
     #protect_from_forgery with :null_session
 
     def test
         render json: { test: "success" }
+    end
+
+    def set_user
+        current_user = session[:user]
+    end
+
+    def logged_in
+        !!session[:user_id]
+    end
+
+    def logout!
+        session.clear
+    end
+
+    helpers do
+    def current_user
+        #nil || User.find(session[:user].id)
+        if session[:user_id].present?
+            User.find(session[:user_id])
+        else
+            nil
+        end
+    end
     end
 end
