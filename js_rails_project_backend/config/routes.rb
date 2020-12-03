913e5ebabfec433e826df864a22d276630fdfe31
Grace S <vocalise@gmail.com>
2020-12-02 21:52:12 -0800

    Updated routes with new actions and paths to authentication

--- js_rails_project_backend/config/routes.rb
+++ js_rails_project_backend/config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
+  #devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/test', to: 'application#test'
-  post '/login', to: 'sessions#create'
+  get '/login', to: 'habits#index'
   post '/signup', to: 'users#create'
   delete '/logout', to: 'sessions#destroy'
   post '/habit', to: 'habits#create'
   get '/habits', to: 'habits#index'
+  #root to: 'users#index'
+  post 'auth_user' => 'authentication#authenticate_user'
 end
