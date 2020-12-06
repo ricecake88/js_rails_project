7189c19c8a0754bed04297e6f7408397972c1b0f
Grace S <vocalise@gmail.com>
2020-12-05 18:05:01 -0800

    Updated User model with devise authentication

--- js_rails_project_backend/app/models/user.rb
+++ js_rails_project_backend/app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
-    has_secure_password
-    validates :first_name, :last_name, presence: true
-    validates :email, presence: true, uniqueness: true, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
-    has_many :habits
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  validates :first_name, :last_name, presence: true
+  validates :email, presence: true, uniqueness: true, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+  has_many :habits
 end
