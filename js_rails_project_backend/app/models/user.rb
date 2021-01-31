b69269c8124ac7782799ddfd6910044430c67267
Grace S <vocalise@gmail.com>
2021-01-30 16:42:07 -0800

    Add validation of confirmation for password and password confirmation

--- js_rails_project_backend/app/models/user.rb
+++ js_rails_project_backend/app/models/user.rb
@@ -5,5 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :first_name, :last_name, presence: true
   validates :email, presence: true, uniqueness: true, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+  validates_confirmation_of :password, message: 'should match confirmation'
+  validates :password_confirmation, presence: true
   has_many :habits
 end
