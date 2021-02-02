0f1cc6c38fada1163dda97a7dd3342dfb6d1f615
Grace S <vocalise@gmail.com>
2021-02-01 23:53:43 -0800

    Simplified create action for users controller

--- js_rails_project_backend/app/controllers/users_controller.rb
+++ js_rails_project_backend/app/controllers/users_controller.rb
@@ -1,21 +1,28 @@
 class UsersController < ApplicationController
 
+    def index
+        render json: {status: true, message: "User#index"}
+    end
+
     def create
         user = User.find_by(:email => params[:email])
-        if !user.present?
+        if !user.nil? or !user.present?
             user = User.new(user_params)
             if user.save
                 session[:user_id] = user.id
-                render json: {status:true, first_name: user.first_name, last_name: user.last_name, email:user.email }
+                render json: {status:true, first_name: user.first_name, last_name: user.last_name, email:user.email, password:user.password, id:user.id }
+                return
             end
         else
             render json: {status: false, message: "User already exists at this e-mail address"}
+            return
         end
+        render json: {status: false, message: user.errors.full_messages}
     end
 
     private
     def user_params
-        params.permit(:firstName, :lastName, :email, :password)
-    end    
+        params.permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end
 
 end
