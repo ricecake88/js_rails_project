e5839c4a45696ec6f5307ec22d806ff4c183641a
Grace S <vocalise@gmail.com>
2021-02-01 23:51:58 -0800

    Rearranged authenticate_user method

--- js_rails_project_backend/app/controllers/authentication_controller.rb
+++ js_rails_project_backend/app/controllers/authentication_controller.rb
@@ -1,10 +1,14 @@
 class AuthenticationController < ApplicationController
     def authenticate_user
         user = User.find_for_database_authentication(email: params[:email])
-        if user.valid_password?(params[:password])
-          render json: payload(user)
+        if user.nil?
+          render json: {errors: 'Cannot find user.'}, status: :unauthorized;
         else
-          render json: {errors: ['Invalid Username/Password']}, status: :unauthorized
+          if user.valid_password?(params[:password])
+            render json: payload(user)
+          else
+            render json: {errors: ['Invalid Username/Password'], status: false}, status: :unauthorized
+          end
         end
     end
 
