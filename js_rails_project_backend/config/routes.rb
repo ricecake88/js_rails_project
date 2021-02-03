5cb8fea678f6631a36dcede9f553250e9f712174
Grace S <vocalise@gmail.com>
2021-02-02 21:21:27 -0800

    Removed double routes to habits#index in routes

--- js_rails_project_backend/config/routes.rb
+++ js_rails_project_backend/config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   #devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/test', to: 'application#test'
-  get '/login', to: 'habits#index'
   post '/signup', to: 'users#create'
   delete '/logout', to: 'sessions#destroy'
   post '/habit', to: 'habits#create'
