275746fc452ea4df295e28d4db98d651a5345282
Grace S <vocalise@gmail.com>
2021-02-16 21:56:15 -0800

    Updated routes with resource for habits

--- js_rails_project_backend/config/routes.rb
+++ js_rails_project_backend/config/routes.rb
@@ -3,10 +3,12 @@ Rails.application.routes.draw do
   #devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/test', to: 'application#test'
-  post '/signup', to: 'users#create'
-  delete '/logout', to: 'sessions#destroy'
-  post '/habit', to: 'habits#create'
-  get '/habits', to: 'habits#index'
-  #root to: 'users#index'
+  resources :users, only: [:create]
+  resources :habits
+  #post '/signup', to: 'users#create'
+  #delete '/logout', to: 'sessions#destroy'
+  #post '/habit', to: 'habits#create'
+  #get '/habits', to: 'habits#index'
   post 'auth_user' => 'authentication#authenticate_user'
+  #delete '/habit/delete/:id', to: 'habits#delete'
 end
