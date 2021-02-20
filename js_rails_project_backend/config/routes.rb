6fb6efd0270d7916cda3ece13082ca80300590c7
Grace S <vocalise@gmail.com>
2021-02-20 14:59:35 -0800

    Removed devise user routes, and added habit_records resources for create

--- js_rails_project_backend/config/routes.rb
+++ js_rails_project_backend/config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  #devise_for :users
   #devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/test', to: 'application#test'
   resources :users, only: [:create]
   resources :habits
+  resources :habit_records, only: [:create]
   #post '/signup', to: 'users#create'
   #delete '/logout', to: 'sessions#destroy'
   #post '/habit', to: 'habits#create'
