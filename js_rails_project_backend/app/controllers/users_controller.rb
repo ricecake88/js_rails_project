b1933fd32ff9ba75945a9dc375721cc74a6f80ef
Grace S <vocalise@gmail.com>
2021-02-27 16:57:24 -0800

    Added error message handling when creating a new user

--- js_rails_project_backend/app/controllers/users_controller.rb
+++ js_rails_project_backend/app/controllers/users_controller.rb
@@ -5,12 +5,16 @@ class UsersController < ApplicationController
     end
 
     def create
+        Rails.logger.info("PARAMS: #{params.inspect}")
         user = User.find_by(:email => params[:email])
         if !user.nil? or !user.present?
             user = User.new(user_params)
             if user.save
                 session[:user_id] = user.id
                 render json: {status:true, first_name: user.first_name, last_name: user.last_name, email:user.email, password:user.password, id:user.id }
+                return
+            else
+                render json: {status: false, message: user.errors.full_messages}
                 return
             end
         else
