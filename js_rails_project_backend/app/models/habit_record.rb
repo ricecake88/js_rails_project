2f0bffaabfc6c133b9fff87939be9f1bb15220ec
Grace S <vocalise@gmail.com>
2021-03-03 20:43:27 -0800

    Updated ranges of dates to query for records under class function of records

--- js_rails_project_backend/app/models/habit_record.rb
+++ js_rails_project_backend/app/models/habit_record.rb
@@ -4,16 +4,22 @@ class HabitRecord < ApplicationRecord
     validate :record_cannot_be_in_future
     validates :time_of_record, presence: true
 
+
+    # TO-DO: FIX LAST MONTH RECORD AND LAST YEAR
     def self.records(params, user_id)
         if params[:range] == "currentMonth"
-            return HabitRecord.where(habit_id: params[:habit_id], time_of_record: Date.today.beginning_of_month..DateTime.current.to_date)
+            return HabitRecord.where(habit_id: params[:habit_id], user_id:user_id,time_of_record: Date.today.beginning_of_month..DateTime.current.to_date).sort_by(&:time_of_record)
+        elsif params[:range] == "lastMonth"
+            return HabitRecord.where(habit_id: params[:habit_id], user_id:user_id, time_of_record: DateTime.last_month..Date.today.beginning_of_month.yesterday).sort_by(&:time_of_record)
         elsif params[:range] == "currentYear"
-            return HabitRecord.where(habit_id: params[:habit_id], time_of_record: Date.today.beginning_of_year..DateTime.current.to_date)
+            return HabitRecord.where(habit_id: params[:habit_id], user_id:user_id, time_of_record: Date.today.beginning_of_year..DateTime.current.to_date).sort_by(&:time_of_record)
+        elsif params[:range] == "lastYear"
+            return HabitRecord.where(habit_id: params[:habit_id], user_id:user_id, time_of_record: DateTime.last_year..Date.today.beginning_of_year).sort_by(&:time_of_record)
         elsif params[:range] == "all"
-            return HabitRecord.where(habit_id: params[:habit_id], user_id:user_id)
+            return HabitRecord.where(habit_id: params[:habit_id], user_id:user_id).sort_by(&:time_of_record)
         else
             # default is the last 7 days
-            return HabitRecord.where(habit_id: params[:habit_id], time_of_record: 1.week.ago.to_date..DateTime.current.to_date)
+            return HabitRecord.where(habit_id: params[:habit_id], user_id:user_id, time_of_record: 1.week.ago.to_date..DateTime.current.to_date).sort_by(&:time_of_record)
         end
     end
 
