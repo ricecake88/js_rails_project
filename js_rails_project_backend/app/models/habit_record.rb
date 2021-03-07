2bc36f93fff2694d4deb7c4837c1e9dfaf3a7eda
Grace S <vocalise@gmail.com>
2021-03-07 14:23:40 -0800

    Fixed current Month and Last Month queries for records

--- js_rails_project_backend/app/models/habit_record.rb
+++ js_rails_project_backend/app/models/habit_record.rb
@@ -8,9 +8,9 @@ class HabitRecord < ApplicationRecord
     # TO-DO: FIX LAST MONTH RECORD AND LAST YEAR
     def self.records(params, user_id)
         if params[:range] == "currentMonth"
-            return HabitRecord.where(habit_id: params[:habit_id], user_id:user_id,time_of_record: Date.today.beginning_of_month..DateTime.current.to_date).sort_by(&:time_of_record)
+            return HabitRecord.where(habit_id: params[:habit_id], user_id:user_id, time_of_record: Date.today.beginning_of_month..DateTime.current.to_date).sort_by(&:time_of_record)
         elsif params[:range] == "lastMonth"
-            return HabitRecord.where(habit_id: params[:habit_id], user_id:user_id, time_of_record: Date.today.last_month..Date.today.beginning_of_month.yesterday).sort_by(&:time_of_record)
+            return HabitRecord.where(habit_id: params[:habit_id], user_id:user_id, time_of_record: Date.today.last_month.beginning_of_month..Date.today.last_month.end_of_month).sort_by(&:time_of_record)
         elsif params[:range] == "currentYear"
             return HabitRecord.where(habit_id: params[:habit_id], user_id:user_id, time_of_record: Date.today.beginning_of_year..DateTime.current.to_date).sort_by(&:time_of_record)
         elsif params[:range] == "lastYear"
