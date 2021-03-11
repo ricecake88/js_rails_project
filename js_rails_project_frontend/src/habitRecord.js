0f55db56f632112d532986999cc39458a9d82dc2
Grace S <vocalise@gmail.com>
2021-03-10 21:13:45 -0800

    Added comments and error handling as well as renamed a few functions to better represent the actions of the code within the function

--- js_rails_project_frontend/src/habitRecord.js
+++ js_rails_project_frontend/src/habitRecord.js
@@ -1,3 +1,4 @@
+/* checks if a record is within the range requested */
 function checkIfInRange(time_of_record, range="last7") {
     const record_as_time = new Date(time_of_record).getTime();
     const today = new Date();
@@ -53,6 +54,7 @@ class HabitRecord {
         return this._timeOfRecord;
     }
 
+    /* create config to send to a post request to log a record */
     static createPostRecordConfig(habit) {
         let record = document.getElementById('habitRecordDateInput' + habit.id);
         return {
@@ -70,6 +72,7 @@ class HabitRecord {
         }
     }
 
+    /* create config to send a get request */
     static createGetRecordsConfig(habit) {
         return {
             method: 'GET',
@@ -79,6 +82,7 @@ class HabitRecord {
         }
     }
 
+    /* create a config to delete a record */
     createDeleteRecordsConfig() {
         return {
             method: 'DELETE',
@@ -95,7 +99,9 @@ class HabitRecord {
         }
     }
 
-    render7Day(habitRecordBoxesTD) {
+    /* if a record is within the last 7 days, create a box
+       and append it to parent element */
+    renderColorBoxIfLast7(habitRecordBoxesTD) {
         if (checkIfInRange(this._timeOfRecord, "last7")) {
             const box = document.createElement("span");
             box.setAttribute("class", "box");
@@ -105,10 +111,15 @@ class HabitRecord {
         }
     }
 
+    /* render the select records returned from server */
     static renderSelectRecords(json, selectElement) {
+
         if (json['status']) {
 
+            // reset select
             selectElement.innerHTML = "";
+
+            // if no records, display no records in select
             if ((json['records'] === undefined) || (json['records'].length === 0)) {
                 const optionRecord = document.createElement("option");
                 optionRecord.innerText = "No Records";
@@ -117,6 +128,8 @@ class HabitRecord {
                 optionRecord.setAttribute("selected", "");
                 selectElement.appendChild(optionRecord);
             } else {
+
+                // render all the records in select
                 json['records'].forEach(record => {
                     const optionRecord = document.createElement("option");
                     optionRecord.setAttribute("value", record['time_of_record']);
@@ -124,43 +137,46 @@ class HabitRecord {
                     optionRecord.innerText = record['time_of_record'];
                     selectElement.appendChild(optionRecord);
                 })
+
+                clearError();
             }
         }
     }
 
+    /* upon login, get all records from server and create
+      new instances of habit records */
     static handleAllRecords(habit) {
-        HabitRecord.getAllRecords(habit).then(function(json) {
+        HabitRecord.getFilteredRecords("all", habit).then(function(json) {
             HabitRecord.initializeRecords(json, habit);
          })
     }
 
-    static get7DayProgress(json, habit) {
+    /* display a record for the cell that shows records from
+       the last 7 days */
+    static render7DayProgress(json, habit) {
         if (json['status'] && json['records'] !== undefined) {
             const habitRecordBoxesTD = document.getElementById("habit7DayProgressDiv" + habit.id);
             habitRecordBoxesTD.innerHTML = "";
             json['records'].forEach(rec => {
 
+                // retrieve related record instance
                 const record = HabitRecord.all.find(r => r.id === rec['id']);
 
                 /* only render if record is found in Habit Record list*/
                 if (record !== undefined)
-                    record.render7Day(habitRecordBoxesTD);
+                    record.renderColorBoxIfLast7(habitRecordBoxesTD);
             })
         }
     }
 
+    /* send request to server to retrieve records for a habit based on a time range */
     static getFilteredRecords(range, habit) {
         const habitRecordsConfigObject = HabitRecord.createGetRecordsConfig(habit);
         return fetchJSON(`${BACKEND_URL}/habit_records?habit_id=${habit.id}&range=${range}`, habitRecordsConfigObject)
         .then(json => json);
     }
 
-    static getAllRecords(habit) {
-        const habitRecordsConfigObject = this.createGetRecordsConfig(habit);
-        return fetchJSON(`${BACKEND_URL}/habit_records?habit_id=${habit.id}&range=all`, habitRecordsConfigObject)
-        .then(json => json);
-    }
-
+    /* initialize instances of existing records upon login */
     static initializeRecords(json, habit) {
         if (json['status'] && json['records'] !== undefined) {
             json['records'].forEach(record => {
@@ -169,6 +185,8 @@ class HabitRecord {
         }
     }
 
+    /* when adding a new record, send request to create a new record to server
+      then render views based on new record */
     static handleNewRecord(habit) {
         HabitRecord.createRecord(habit).then(json => {
             HabitRecord.newRecord(json, habit);
@@ -176,12 +194,14 @@ class HabitRecord {
 
     }
 
+    /* post server request to create a new record */
     static createRecord(habit) {
-        const configObject = HabitRecord.createPostRecordConfig(habit);
-        return fetchJSON(`${BACKEND_URL}/habit_records`, configObject)
+        return fetchJSON(`${BACKEND_URL}/habit_records`, HabitRecord.createPostRecordConfig(habit))
         .then(json => json)
     }
 
+    /* render new record by creating a new habit record instance, then updating
+        last 7 day column, list of records and summary based on the new record information */
     static newRecord(json, habit) {
         if (json['status'] == true) {
 
@@ -191,26 +211,33 @@ class HabitRecord {
             // update last 7 day cell if record is in the last 7 days
             habit.handle7DayRecords();
 
+            // render list of records again after creation if in
+            // selected range
             habit.handleSelectedRecords();
+
             renderHabitSummary(habit.user);
+            clearError();
 
         } else {
             document.getElementById("error").innerText = json['errors'];
         }
     }
 
+    /* delete record based on record passed in and update views based on deleted record */
     static handleDeleteRecord(record) {
         const recordId = parseInt(record.id.match(/[0-9]+/)[0])
         const recordObj = HabitRecord.all.find(r => r.id === recordId)
         recordObj.deleteRecord().then(json => recordObj.renderDeleteRecords(json))
     }
 
+    /* delete request for a record to server */
     deleteRecord() {
         const deleteRecordConfig = this.createDeleteRecordsConfig();
         return fetchJSON(`${BACKEND_URL}/habit_records/${this.id}`, deleteRecordConfig)
             .then(json => json)
     }
 
+    /* render the page based on the deleted record */
     renderDeleteRecords(json) {
         if (json['status']) {
 
@@ -222,6 +249,8 @@ class HabitRecord {
             this.habit.handleSelectedRecords();
 
             renderHabitSummary(this.habit.user);
+        } else {
+            displayError(json['errors']);
         }
     }
 
