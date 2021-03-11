9dece7ab80c510d2b7cb9447ecc238c02a9610da
Grace S <vocalise@gmail.com>
2021-03-10 21:10:14 -0800

    Updated rendering of error messages, adding statuses and changed destroy method to handle errors

--- js_rails_project_backend/app/controllers/habits_controller.rb
+++ js_rails_project_backend/app/controllers/habits_controller.rb
@@ -1,10 +1,10 @@
 class HabitsController < ApplicationController
 
-    before_action :authenticate_request!
+    before_action :authenticate_request
 
     def index
         unless logged_in?
-            render json: {status: false}
+            render json: {status: false}, status: 401
         else
             habits = Habit.where(:user_id => @current_user.id).order("name ASC")
             render json: {status: true, first_name: @current_user.first_name, last_name: @current_user.last_name, email: @current_user.email, password: @current_user.encrypted_password, id: @current_user.id, habits: habits}
@@ -12,43 +12,51 @@ class HabitsController < ApplicationController
     end
 
     def create
-        if logged_in?
-            Rails.logger.info("PARAMS: #{params.inspect}")
+        unless logged_in?
+            render json: {status: false, errors: self.errors.full_messages.push(["Not Logged In"])}, status: 401
+        else
+            #Rails.logger.info("PARAMS: #{params.inspect}")
             habit = Habit.find_by(:name => params[:name], :user_id => @current_user.id)
             if !habit.present?
                 habit = Habit.new(habit_params)
                 if habit.save
                     habit.user = @current_user
-                    render json: {status: true, message: "habit saved", habit: habit}
+                    render json: {status: true, habit: habit}
                 else
                     render json: {status: false, errors: habit.errors.full_messages}
+                    return
                 end
-
             else
-                render json: {status: false, errors: habit.errors.full_messages.push("Habit Already Exists")}
+                render json: {status: false, errors: habit.errors.full_messages.push(["Habit Already Exists"])}
             end
-        else
-            render json: {status: false, errors: "Not Logged In"}
         end
     end
 
     def update
         unless logged_in?
-            render json: {status: false}
+            render json: {status: false, errors: self.errors.full_messages.push(["Not Logged In"])}, status: 401
         else
             habit = Habit.find(params[:id])
-            if habit.update(habit_params)
+            if habit.present? && habit.update(habit_params)
                 render json: {status: true, habit: habit}
+            else
+                render json: {status: false, errors: habit.errors.full_messages}
             end
         end
     end
 
     def destroy
-        if logged_in?
+        unless logged_in?
+            render json: {status: false, errors: self.errors.full_messages.push(["Not Logged In"])}, status: 401
+        else
             name = params['name']
-            habit = Habit.find(params[:id]).habit_records.destroy_all
-            Habit.find(params[:id]).destroy
-            render json: {status: true, id: params['id'], message: name + " has been deleted." };
+            habit = Habit.find(params[:id])
+            if habit.present? && habit.habit_records.destroy_all && habit.destroy
+                render json: {status: true, id: params['id']};
+            else
+                render json: {status: false, errors: "Error deleting habit"}
+            end
+
         end
     end
 
