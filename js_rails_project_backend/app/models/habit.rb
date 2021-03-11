3166a3b6cb1c718a2f292fe83da53ab62819e2c4
Grace S <vocalise@gmail.com>
2021-03-10 21:11:20 -0800

    Added validation of habit name in habit model

--- js_rails_project_backend/app/models/habit.rb
+++ js_rails_project_backend/app/models/habit.rb
@@ -1,5 +1,5 @@
 class Habit < ApplicationRecord
     belongs_to :user
-    validates :name, presence: true
+    validates :name, presence: true, format: {with: /\A[\w\s]+\s*[\w\s\.&!\-]*\Z/ }
     has_many :habit_records
 end
