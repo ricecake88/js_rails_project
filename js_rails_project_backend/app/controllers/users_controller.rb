fd3206f94c40d13591499bb0389298d24b0faad6
Grace S <vocalise@gmail.com>
2021-03-10 21:10:56 -0800

    Modified handling of errors and updated error messages

--- js_rails_project_backend/app/controllers/users_controller.rb
+++ js_rails_project_backend/app/controllers/users_controller.rb
@@ -5,23 +5,20 @@ class UsersController < ApplicationController
     end
 
     def create
-        Rails.logger.info("PARAMS: #{params.inspect}")
+        #Rails.logger.info("PARAMS: #{params.inspect}")
         user = User.find_by(:email => params[:email])
         if !user.nil? or !user.present?
             user = User.new(user_params)
             if user.save
-                session[:user_id] = user.id
+                #session[:user_id] = user.id
                 render json: {status:true, first_name: user.first_name, last_name: user.last_name, email:user.email, password:user.password, id:user.id }
-                return
-            else
-                render json: {status: false, error: user.errors.full_messages}
                 return
             end
         else
-            render json: {status: false, error: "User already exists at this e-mail address"}
+            render json: {status: false, errors: user.errors.full_messages.push(["User already exists at this e-mail address"])}
             return
         end
-        render json: {status: false, error: user.errors.full_messages}
+        render json: {status: false, errors: user.errors.full_messages}
     end
 
     private
