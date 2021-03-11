9213353c8207c3fbe8c334913b3a0dee1e3c4ffb
Grace S <vocalise@gmail.com>
2021-03-10 21:20:15 -0800

    Added schema

---
+++ js_rails_project_backend/db/schema.rb
@@ -0,0 +1,48 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_03_04_044913) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "habit_records", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "habit_id"
+    t.date "time_of_record"
+  end
+
+  create_table "habits", force: :cascade do |t|
+    t.string "name"
+    t.string "frequency_mode"
+    t.integer "streak_counter"
+    t.integer "streak_level"
+    t.integer "num_for_streak"
+    t.integer "user_id"
+    t.string "color"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+end
