d899588179f5c100ccf038ec292c17cb4d954897
Grace S <vocalise@gmail.com>
2021-03-10 21:09:06 -0800

    Added statuses and error messages to jsons

--- js_rails_project_backend/app/controllers/habit_records_controller.rb
+++ js_rails_project_backend/app/controllers/habit_records_controller.rb
@@ -1,30 +1,30 @@
 class HabitRecordsController < ApplicationController
 
-    before_action :authenticate_request!
+    before_action :authenticate_request
     def index
-        Rails.logger.info("PARAMS: #{params.inspect}")
+        #Rails.logger.info("PARAMS: #{params.inspect}")
         unless logged_in?
-            render json: { message: "Error not logged in"}
+            render json: { errors: ["Error not logged in"]}
         else
             records = HabitRecord.records(params, @current_user.id)
-            render json: { status: true, records: records, range: params[:range], time: 1.week.ago.to_date, user: @current_user}
+            render json: { status: true, records: records, range: params[:range], user: @current_user}
         end
     end
 
     def create
-        Rails.logger.info("PARAMS: #{params.inspect}")
+        #Rails.logger.info("PARAMS: #{params.inspect}")
         unless logged_in?
             render json: {status: false, message: "Blah"}
         else
             habit = HabitRecord.find_by(:habit_id => params[:habit_id], :user_id => @current_user.id, :time_of_record => params[:time_of_record])
             if habit.present?
-                render json: {status: false, errors: habit.errors.full_messages.push("Already Recorded")}
+                render json: {status: false, errors: habit.errors.full_messages.push(["Already Recorded"])}
             else
                 habit_record = HabitRecord.new(habit_record_params)
                 if habit_record.save
-                    render json: {status: true, record: habit_record}
+                    render json: {status: true, record: habit_record}, status: 200
                 else
-                    render json: {status: false, errors: habit_record.errors.full_messages}
+                    render json: {status: false, errors: habit_record.errors.full_messages}, status: 422
                 end
             end
         end
@@ -33,10 +33,10 @@ class HabitRecordsController < ApplicationController
 
     def destroy
         habit_record = HabitRecord.find(params[:id])
-        if habit_record.destroy
+        if habit_record.present? && habit_record.destroy
             render json: {status: true, id: params[:id]}
         else
-            render json: {status: false, message: "Failed"}
+            render json: {status: false, errors: habit_record.errors.full_messages.push(["Error Deleting Record"])}
         end
     end
 
