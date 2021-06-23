3ea2bd4a83fe95a9f33c6bd014224c8eb7f0c45a
Grace S <vocalise@gmail.com>
2021-06-23 00:58:06 -0700

    Remove loading up lib in application.rb

--- config/application.rb
+++ config/application.rb
@@ -22,8 +22,6 @@ module JsRailsProjectBackend
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    config.eager_load_paths << Rails.root.join('lib')
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
